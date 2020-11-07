b25e313b0b10b96c58c6f3f0e95588f81ef97401
13NOONE37 <73988744+13NOONE37@users.noreply.github.com>
2020-11-07 11:47:55 +0100

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,28 @@
+var menu_click=0;
+
+function menu()
+{/*
+ if(menu_click==0){
+     document.getElementByClassName('square')[0].style['transform'].style['transform']='rotate(45deg)';
+      alert("if");
+     for(var i=0; i<7; i++)
+         {  
+         document.getElementByClassName('square')[i].style['rotate']='45deg';
+         }
+     menu_click++;
+ } 
+    if(menu_click==1){
+      alert("elseif");
+     for(var i=0; i<7; i++)
+         {
+         document.getElementByClassName('square')[i].style['transform']='rotate(0deg)';
+         }
+     menu_click++;
+ }
+    if(menu_click==2){
+        menu_click++;
+}*/
+}
+
+
+
